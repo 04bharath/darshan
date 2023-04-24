b1efaca9c4810d7087968805a448b11f389e5f3c
04bharath <129049143+04bharath@users.noreply.github.com>
2023-04-24 15:04:41 +0530

    Update one.java

--- one.java
+++ one.java
@@ -1,6 +1,6 @@
 public class one{
 	public static void main (String[] args)
 	{
-		System.out.println("hello world change 2 issue 2");
+		System.out.println("hello world change 2 issue 2 tumkur smartcity");
 	}
 }
